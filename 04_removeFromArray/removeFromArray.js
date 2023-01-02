57fe195cc56052d20e53eb5288d39563ad3e2298
RezonHamiti <rezon.hamiti@gmail.com>
2023-01-02 12:25:02 +0100

    excercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...value) {
+    let newArray = array.filter(function(item){
+        return !value.includes(item);
+    });
+    return newArray;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
